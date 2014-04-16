5447868d4e791d2aa3c6a52778fffd1674f255aa
Tim <ma3tsa@mail.ru>
2014-04-16 17:39:46 +0400

    Initial Commit

---
+++ CTConsultingTestTak/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CTConsultingTestTak
+//
+//  Created by Ultimatum on 16.04.14.
+//  Copyright (c) 2014 Timur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
